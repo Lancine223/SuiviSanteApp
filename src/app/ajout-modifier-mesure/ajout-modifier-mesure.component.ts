277599da234a7e5045928ce3ab2ccfc4ab07b954
Lancine223 <lacinekeita20@gmail.com>
2023-08-22 22:03:07 +0000

    material

---
+++ src/app/ajout-modifier-mesure/ajout-modifier-mesure.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ajout-modifier-mesure',
+  templateUrl: './ajout-modifier-mesure.component.html',
+  styleUrls: ['./ajout-modifier-mesure.component.css']
+})
+export class AjoutModifierMesureComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AjoutModifierMesureComponent } from './ajout-modifier-mesure/ajout-modifier-mesure.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AjoutModifierMesureComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDialogModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
